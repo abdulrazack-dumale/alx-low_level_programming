a80b0fe84c3f5fe3afcc1e0534f4d29538b363ad
abdulrazack-dumale <musharafabdul@gmail.com>
2023-02-23 03:18:50 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isupper - checks for uppercase character
  * @c: Variable text
@@ -8,15 +9,13 @@
 int _isupper(int c)
 {
 
-
 	 if (c >= 'A' && c <= 'Z')
 		  {
-			   return (1);
-			    }
+	        return (1);
+                  }
 	  else
 	  {
-		   return (0);
+                return (0);
 	  }
-
 }
 
