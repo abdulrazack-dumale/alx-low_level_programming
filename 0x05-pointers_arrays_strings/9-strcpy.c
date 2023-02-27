837347b078bdcdd3dbc2f84b14bfdfa29aec09fb
abdulrazack-dumale <musharafabdul@gmail.com>
2023-02-27 02:37:28 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,7 @@ nclude "main.h"
 #include "main.h"
 
 /**
- * char *_strcpy - a function that copies the string pointed to by src
+ * char _strcpy - a function that copies the string pointed to by src
  * @dest: copy to
  * @src: copy from
  * Return: string
